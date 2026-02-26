d0857d9b362c18c6f9ac7686975d4f2455b5130c
unknown <papariyaswanthreddy@gmail.com>
2026-02-26 16:19:04 +0530

    UC3 - Reverse Palindrome Logic

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+        String reversed = "";
+
+        // Reverse string using loop
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        // Compare original and reversed string
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
+    }
+}
